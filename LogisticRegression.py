238aa9c56f32e7b2b5d0e2da4833a0f4b880c40b
chanwul <hwool2000@naver.com>
2018-10-30 17:58:59 +0900

    10/30
    10/30

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,4 +1,6 @@
+import csv
 import numpy as np
+from sklearn.metrics import classification_report
 
 class LogisticRegression():
     def __init__(self, x, y):
@@ -9,7 +11,7 @@ class LogisticRegression():
         self.w = np.zeros(self.num_feature)
 
     def _sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
+        return 1.0 / (1.0 + np.exp(-z))
 
     def predict(self, x):
         temp2 = np.dot(x, self.w)
@@ -21,7 +23,55 @@ class LogisticRegression():
 
     def train(self, lr, epoch):
         for i in range(0, epoch):
-            temp = np.dot(self.x,self.w)
-            grad = np.dot((self.y - self._sigmoid(temp)),self.x)
+            temp = np.dot(self.x, self.w)
+            grad = np.dot((self.y - self._sigmoid(temp)), self.x)
             self.w = self.w + lr * grad
         return self.w
+
+
+def data_load(data):
+    y = []
+    x = []
+    file = None
+    if data == 'train':
+        file = './train.csv'
+
+    elif data == 'test':
+        file = './test.csv'
+
+    with open(file, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        next(csv_reader)
+        for row in csv_reader:
+            y.append(float(row[0]))
+            feature = [1] # bias
+            for i in range(1, 9):
+                feature.append(float(row[i]))
+            x.append(feature)
+        return np.array(x, dtype="f8"), np.array(y, dtype="f8")
+
+
+def eval(prediction, y):
+    hit = 0
+    for i, p in enumerate(prediction):
+        if p == y[i]:
+            hit += 1
+    print(classification_report(y, prediction, target_names=['negative', 'positive']))
+    print(hit/len(y))
+
+
+def main():
+   X_train, Y_train = data_load('train')
+   model = LogisticRegression(X_train, Y_train)
+   model.train(0.000001, 50000)  # learning rate, epoch
+   X_test, Y_test = data_load('test')
+   prediction = model.predict(X_test)
+   for i in range(prediction.size):
+       print(prediction[i])
+   eval(prediction, Y_test)
+
+
+if __name__ == "__main__":
+    main()
+
+
--- kmeans.py
+++ kmeans.py
@@ -5,7 +5,7 @@ from sklearn.metrics import classification_report
 
 def data_load():
     x = pd.read_csv("./train.csv")
-    data = x.values[:, 1:8]
+    data = x.values[:, 1:9]
     label = x.values[:, 0]
     return data, label
 
@@ -15,7 +15,7 @@ def eval(prediction, diabetes):
     for i, p in enumerate(prediction):
         if p == diabetes[i]:
             hit += 1
-    print(classification_report(diabetes, prediction, target_names=['No', 'Yes']))
+    print(classification_report(diabetes, prediction, target_names=['negative', 'positive']))
     print(hit / prediction.shape[0])
 
 
@@ -46,25 +46,33 @@ def main():
     data, diabetes = data_load()
     k = 2
     num = data.shape[0]
-    centroid = init(data, k)
-    cur = deepcopy(centroid)
-    clusters = np.zeros(num)
-    previous = np.zeros(centroid.shape)
-    distances = np.zeros((num, k))
+    result = np.zeros(num)
+    for n in range(1000):
+        centroid = init(data, k)
+        cur = deepcopy(centroid)
+        clusters = np.zeros(num)
+        previous = np.zeros(centroid.shape)
+        distances = np.zeros((num, k))
 
-    while np.linalg.norm(cur - previous) != 0:
-        for i in range(k):
-            distances[:, i] = measure(data, i, centroid)
-        clusters = np.argmin(distances, axis=1)
-        for i in range(k):
-            if clusters[i] == i:
-                cur[i] = np.mean(data[i], axis=0)
-        previous = deepcopy(cur)  # update previous
+        while np.linalg.norm(cur - previous) != 0:
+            for i in range(k):
+                distances[:, i] = measure(data, i, centroid)
+            clusters = np.argmin(distances, axis=1)
+            for i in range(k):
+                if clusters[i] == i:
+                    cur[i] = np.mean(data[i], axis=0)
+            previous = deepcopy(cur)  # update previous
+        negate(clusters, num)
+        result += clusters
 
-    negate(clusters, num)
-    eval(clusters, diabetes)
     for i in range(num):
-        print (clusters[i])
+        if result[i] >= 500:
+            result[i] = 1
+        else:
+            result[i] = 0
+    eval(result, diabetes)
+    for i in range(num):
+        print (result[i])
 
 
 if __name__ == "__main__":
